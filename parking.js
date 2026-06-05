323c9e9f12fd41de5f2318fcc0ef096c9598c8fc
dagasystem <dagasystem.iizuka@gmail.com>
2026-06-05 10:59:11 +0900

    Create parking.js

---
+++ parking.js
@@ -0,0 +1,18 @@
+window.SHOPS = [
+
+  {
+    id:0,
+    type:"image",
+    image:"parking1",
+    src:"parking1.png",
+    url:"#",
+    messages:[
+      "あいタウン駐車場",
+      "最初の１時間無料",
+      "以降30分ごとに100円"
+    ]
+  },
+
+
+
+];
